867ee8ba2b911089923513415253bcf3ccd57be6
Joel Lopez <jolopez.satx@gmail.com>
2024-02-10 14:42:24 -0600

    fixed picture routes

--- client/src/pages/Restaurant.jsx
+++ client/src/pages/Restaurant.jsx
@@ -5,27 +5,27 @@ const restaurant = () => {
   const projects = [
     {
       title: 'La Panaderia',
-      image: '/public/images/la-panaderia.jpg',
+      image: '/images/la-panaderia.jpg',
       deployedLink: 'https://twitter.com/breadcultura',
     },
     {
       title: 'Curry Boys BBQ',
-      image: '/public/images/curry-boys-bbq.webp',
+      image: '/images/curry-boys-bbq.webp',
       deployedLink: 'https://www.yelp.com/biz/curry-boys-bbq-san-antonio-2',
     },
     {
       title: 'Zaatar Lebanese Restaurant',
-      image: '/public/images/zaatar.png',
+      image: '/images/zaatar.png',
       deployedLink:'https://www.yelp.com/biz/zaatar-lebanese-grill-san-antonio',
     },
     {
       title: 'Simis Indian Cuisine',
-      image: '/public/images/simis.jpg',
+      image: '/images/simis.jpg',
       deployedLink: 'https://www.yelp.com/biz/simis-india-cuisine-san-antonio',
     },
     {
       title: 'Pollos Asados Los Nortenos',
-      image: '/public/images/pollos.jpg',
+      image: '/images/pollos.jpg',
       deployedLink: 'https://www.yelp.com/biz/pollos-asados-los-norte%C3%B1os-san-antonio',
     },
   ];
